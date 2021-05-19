f5af946f90551faf8bff69179d7e10d9ce6e49ad
chris <838311583@qq.com>
2021-05-19 17:16:34 +0800

    Update implement.py

--- Recommendation with explicit feedback (CF, CR)/SVDpp/implement.py
+++ Recommendation with explicit feedback (CF, CR)/SVDpp/implement.py
@@ -1,5 +1,7 @@
 
 import pandas as pd
+import math 
+import numpy as np
 
 n = 943
 m = 1682
@@ -99,7 +101,10 @@ def SVDpp_initialization(d):
     return r_mean, b_usr, b_item, U, V, W, I_u
 
 def SVDpp_prediction(mu, b_u, b_i, U_u, V_i_T, U_virtual_u):
-    return mu + b_u + b_i + U_u @ V_i_T + U_virtual_u @ V_i_T
+    prediction = mu + b_u + b_i + U_u @ V_i_T + U_virtual_u @ V_i_T
+    if prediction > 5: prediction = 5
+    if prediction < 1: prediction = 1
+    return prediction
 
 def SVDpp(alpha_u, alpha_v, alpha_w, beta_u, beta_v, gamma, d, T):
     mu, b_u, b_i, U, V, W, I_u = SVDpp_initialization(d)
@@ -122,7 +127,7 @@ def SVDpp(alpha_u, alpha_v, alpha_w, beta_u, beta_v, gamma, d, T):
             delta_b_i = -e_ui + beta_v * b_i[item_id]
             delta_U_u = -e_ui * V[item_id - 1] + alpha_u * U[usr_id - 1]
             delta_V_i = -e_ui * (U[usr_id - 1] + U_virtual_u) + alpha_v * V[item_id - 1]
-            delta_W_i_v = np.zeros([m])
+            delta_W_i_v = np.zeros([m, d])
             for v_i in I_u[usr_id]:
                 delta_W_i_v[v_i - 1] = -e_ui / math.sqrt(abs(sum(I_u[usr_id]))) * V[item_id - 1] + alpha_w * W.reshape(m, d)[v_i - 1]
             
@@ -144,6 +149,7 @@ def SVDpp(alpha_u, alpha_v, alpha_w, beta_u, beta_v, gamma, d, T):
         item_id = row[1]
         rating = row[2]
         r_ui_prediction = RSVD_prediction(mu, b_u[usr_id], b_i[item_id], U[usr_id - 1].reshape(1, d), V[item_id - 1].reshape(d, 1))
+        
         bias_sum += abs(r_ui_prediction - rating)
         square_bias_sum += (r_ui_prediction - rating) ** 2
 
@@ -155,7 +161,15 @@ def SVDpp(alpha_u, alpha_v, alpha_w, beta_u, beta_v, gamma, d, T):
     return 
 
 def main():
+    alpha_u = alpha_v = alpha_w = beta_u = beta_v = 0.01
+    gamma = 0.01
+    T = 100
+    d = 20
 
+    print("SVD++")
+    MAE, RMSE = SVDpp(alpha_u, alpha_v, alpha_w, beta_u, beta_v, gamma, d, T)
+    print(MAE)
+    print(RMSE)
     return
 
 if __name__ == '__main__':
